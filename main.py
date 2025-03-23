90d35711fcfcc6cdae1989de414847dafc7fd162
Jeba-1 <jebajayapal.ias@gmail.com>
2025-03-23 21:35:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import numpy as np
 from PIL import Image
 
 # FastAPI Endpoint URL (Change this to your Render API URL)
-API_URL = "https://automated-classification-of-kidney.onrender.com/docs#/default/predict_predict__post"
+API_URL = "https://automated-classification-of-kidney.onrender.com/"
 
 st.title("Kidney Condition Classification")
 
